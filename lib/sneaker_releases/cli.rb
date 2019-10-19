d76f6a43aa085d1eb0b67cce4fbc8ded249e2737
Christian Perez <perezchristian1012@gmail.com>
2019-10-19 04:29:58 +0000

    updated cli file

--- lib/sneaker_releases/cli.rb
+++ lib/sneaker_releases/cli.rb
@@ -1,18 +1,18 @@
 class SneakerReleases::CLI 
   
   def call
-   puts "Welcome to the Sneaker Release page!"
-   get_sneakers
-   #get_release_date
+    puts "Welcome to the Sneaker Release page!"
+    get_sneakers
+    list_sneakers
   end
   
   
-  def sneakers
+  def get_sneakers
     @sneakers = ["Nike Air Force 1 Low What The NYC", "Patta x Air Jordan 7 Icicle", "Puma Clyde Hardwood White Black", "BlackEyePatch x Reebok Daytona DMX II", "Nike Little Posite One Wolf Grey Graffiti", "Air Jordan 10 Seattle", "adidas Harden Vol 4 Collegiate Royal", "adidas Harden Vol 4 Scarlet", "adidas Harden Vol 4 Grey", "adidas Harden Vol 4 Core Black", "adidas Harden Vol 4 Glow Green", "Air Jordan 1 Retro High OG WMNS Fearless"]
   end
   
-  def get_sneakers
-    @sneakers.each.with_index do |index, sneaker|
+  def list_sneakers
+    @sneakers.each.with_index do |sneaker, index|
       puts "#{index + 1}. #{sneaker}"
     end
   end
